ce4f1471aee82be7fc4f10b6a0baff8deaad6015
KangbeenKo <sweetrainforyou@gmail.com>
2021-02-01 19:27:54 +0900

    Add 0201

--- PS/BOJ_11055/BOJ11055_KangbeenKo.py
+++ PS/BOJ_11055/BOJ11055_KangbeenKo.py
@@ -1,14 +1,22 @@
-n = int(input())
-nums = list(map(int, input().split()))
-answer = [0] * n
+# n = int(input())
+# nums = list(map(int, input().split()))
+# answer = [0] * n
 
-for i in range(n):
-    temp = 0
+# for i in range(n):
+#     temp = 0
 
-    for j in range(i):
-        if nums[j]<nums[i]:
-            temp = max(temp, answer[j])
+#     for j in range(i):
+#         if nums[j]<nums[i]:
+#             temp = max(temp, answer[j])
     
-    answer[i] = temp + nums[i]
+#     answer[i] = temp + nums[i]
 
-print(max(answer))
+# print(max(answer))
+
+n, k = map(int, input().split())
+value = 0
+coin = []
+for _ in range(n):
+    coin.append(input())
+coin.sort()
+# print(coin)
