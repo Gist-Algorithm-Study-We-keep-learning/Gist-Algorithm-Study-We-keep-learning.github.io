4a1394b2552fe67656dcbb5005b556e0fdaecc20
leessunj <sj06152@gmail.com>
2021-02-07 16:45:38 +0900

    add 2293,2748,9095,9625,11726

--- sunjae/B2293.cpp
+++ sunjae/B2293.cpp
@@ -6,15 +6,20 @@ using namespace std;
 int main(){
     int n,k;
     scanf("%d%d",&n,&k);
-    int data[n];
+    int data[n],dp[k+1]={0,};
     for(int i=0;i<n;i++){
         cin>>data[i];
     }
     sort(data,data+n);
 
-    for(int i=0;i<k/data[n-1];i++){
-        
-
+    for(int j=0;j<n;j++){
+        for(int i=data[j];i<=k;i++){
+            if(data[j]==i) dp[i]+=1;
+            else dp[i]+=dp[i-data[j]]; 
+           // cout<<dp[i]<<' ';
+        }
+      //  cout<<'\n';
     }
+    cout<<dp[k];
     return 0;
 }
