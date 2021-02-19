54272ecfcfa54d8506d5d559ec6c7663007dc0d6
KangbeenKo <sweetrainforyou@gmail.com>
2021-02-19 22:17:59 +0900

    0219 Refactoring

--- KangbeenKo(Python)/3. 나누어 떨어지는 숫자 배열.py
+++ KangbeenKo(Python)/3. 나누어 떨어지는 숫자 배열.py
@@ -1,3 +1,4 @@
+# First Code
 def solution(arr, divisor):
     answer = []
     for i in range(len(arr)):
@@ -10,14 +11,23 @@ def solution(arr, divisor):
     answer.sort()
     return answer
 
-arr = []
+# Refactoring 1 (21.02.19)
+def solution(arr, divisor):
+    answer = []
+    for i in arr: # 반복문에 대한 개념이 부족한 상태였기에 했던 실수(?)
+        if i%divisor==0:
+            answer.append(i)
+        else:
+            answer
+    if len(answer)==0:
+        answer.append(-1)
+    answer.sort()
+    return answer
 
-while(1):
-    Input = int(input('Enter: '))
-    if Input != -1:
-        arr.append(Input)
-    else:
-        break
-
-divisor = int(input("Divide for what?: "))
-print(solution(arr, divisor))
+# Refactoring 2 (21.02.19)
+def solution(arr, divisor): 
+    return sorted([n for n in arr if n%divisor == 0]) or [-1] # list comprehension 이용
+    # arr의 원소 n이 divisor에 의해 나눠 떨어지면 리스트에 삽입
+    # 그렇지 않으면(or 앞이 전부 거짓이면) -1 삽입
+    # 만들어진 리스트 내 원소를 정렬하여 'str 형태의 원소'를 가진 '리스트'로 반환
+    # ex - sorted([1,3,2,7,5]) >> ["1","2","3","5","7"]
--- KangbeenKo(Python)/5. 문자열 내 마음대로 정렬하기.py
+++ KangbeenKo(Python)/5. 문자열 내 마음대로 정렬하기.py
@@ -1,3 +1,4 @@
+# First Code
 def solution(strings, n):
     answer = []
     for i in range(len(strings)):
@@ -9,3 +10,20 @@ def solution(strings, n):
         answer.append(strings[i])
         
     return answer
+
+# Refactoring 1 (21.02.19)
+def solution(strings, n):
+    strings = sor[i[n]+i for i in strings]
+    strings.sort()
+    return [x[1:] for x in strings]
+
+# Refactoring 2 (21.02.19)
+def solution(strings, n):
+    strings = sorted([i[n]+i for i in strings])
+    return [x[1:] for x in strings]
+
+# Refactoring 3 (21.02.19)
+def solution(strings, n):
+    return sorted(strings, key=lambda x: x[n])
+    # sorted(정렬대상, key = 정렬 기준)
+    # lambda 인자:표현식 >> 인자를 표현식의 형태로 바꿔줌
