b1bf4333755b6434504955ae547e5acaf9ccc7e4
leessunj <77258405+leessunj@users.noreply.github.com>
2021-03-14 20:59:36 +0900

    modify 10816
    이전 코드를 넣었길래 다시 올바른 코드를 넣었습니다

--- sunjae/BOJ_10816_sunjae.cpp
+++ sunjae/BOJ_10816_sunjae.cpp
@@ -3,44 +3,20 @@
 
 using namespace std;
 
-int find(int k,long a[],int n){
-    int l=0,r=n-1,m;
-    while(l<=r){
-        m=(l+r)/2;
-        if(k<a[m]) r=m-1;
-        else if(k>a[m]) l=m+1;
-        else if(m>1&&k!=a[m-1]) break;
-        else if(m>1&&k==a[m-1]) r=r-2;
-    }
-    if(a[m]==k) return m;
-    else return -1;
-}
-
 int main(){
-    int n,m,cnt,k;
+    int n,m;
     cin>>n;
-    long a[n];
+    int a[n];
     for(int i=0;i<n;i++) cin>>a[i];
-    sort(a,a+n);
     cin>>m;
-    long b[m];
+    int b[m];
     for(int i=0;i<m;i++) cin>>b[i];
+    
+    sort(a,a+n);
     for(int i=0;i<m;i++){
-        k=find(b[i],a,n);
-        //printf("(%d)",k);
-        cnt=0;
-        if(k==-1){ 
-            printf("0 ");
-        }
-        else{
-            
-            while(b[i]==a[k]){
-                cnt++;
-                k++;
-            }
-            printf("%d ",cnt);
-        }    
-        
+        auto low=lower_bound(a,a+n,b[i]);
+        auto up=upper_bound(a,a+n,b[i]);
+        cout<<up-low<<" ";
     }
     return 0;
 }
